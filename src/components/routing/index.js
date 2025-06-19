1dfdf751783833f8ac014de44ad60f8b961a9073
Paulo Galarza <paulo-123@hotmail.es>
2025-06-19 15:04:49 -0500

    Configuracion de entry point de componente y routing

---
+++ src/components/routing/index.js
@@ -0,0 +1,4 @@
+import "./link.js";
+import "./route.js";
+import "./viewport.js";
+import "./router.js";
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import "./components/button.js";
+import "./components/button/button.js";
+import "./views/home/home.js";
+import "./components/navbar/navbar.js";
+import "./views/page2/page2.js";
+import "./views/page3/page3.js";
+import "./components/routing/index.js";
